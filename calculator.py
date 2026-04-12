bfb45336932b2e9127a38382dec03b3436a104ee
Count-von-Count <HinsonJW@outlook.com>
2026-04-12 14:52:16 -0400

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -9,8 +9,18 @@ def subtract(a, b):
 def multiply(a, b):
     return a * b
 
+def divide(a, b):
+    return a / b
 
+def logarithm(a, b):
+    try:
+        if b == 0 or a == 0:
+            raise ValueError
+        else:
+            return math.log(b, a)
+    except ValueError:
+        print("Base and value must be positive.")
+        return 0
 
-
-
-
+def exponent(a, b):
+    return a**b
