94cfeb89cda8f3e8f77bec52484efde1d04f3299
Count-von-Count <HinsonJW@outlook.com>
2026-04-13 21:07:01 -0400

    modified calculator

--- calculator.py
+++ calculator.py
@@ -1,51 +1,35 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
+import math
 
-One function per operation, in order.
-"""
-# First example
 def add(a, b): 
     return a + b
 
-def subtract(a, b):
+def sub(a, b):
     return a - b
 
 def mul(a, b):
     return a * b
 
 def div(a, b):
-    try:
-        if a == 0:
-            raise ZeroDivisionError
-        else:
-            return b / a
-    except ZeroDivisionError:
-        print("Can't divide by zero.")
-        return 0
+    if a == 0:
+        raise ZeroDivisionError
+    else:
+        return b / a
 
 def logarithm(a, b):
-    try:
-        if b == 0 or a == 0:
-            raise ValueError
-        else:
-            return math.log(b, a)
-    except ValueError:
-        print("Base and value must be positive.")
-        return 0
+    if b == 0 or a == 0:
+        raise ValueError
+    else:
+        return math.log(b, a)
 
 def exp(a, b):
     return a**b
 
 def square_root(a):
-    try:
-        if a < 0:
-            raise ValueError
-        else:
-            return math.sqrt(a)
-    except ValueError:
-        print("Base must be positive.")
-        return 0
+    if a < 0:
+        raise ValueError
+    else:
+        return math.sqrt(a)
+
 
 def hypotenuse(a, b):
-    return math.hypot(a, b)
+    return  math.hypot(a, b)
